73a0e7d58d63aa284b249876d9505e623d2821b9
5OO <git.hub@myrk.eu>
2024-01-11 13:26:02 +0200

    Java project setup - initial commit

---
+++ src/main/java/org/tims/aliasnetwork/AliasNetworkApplication.java
@@ -0,0 +1,13 @@
+package org.tims.aliasnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliasNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliasNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tims/aliasnetwork/AliasNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tims.aliasnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliasNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
