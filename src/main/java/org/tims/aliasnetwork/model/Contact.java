e09f3b5fb2052f7f9fed0047a77514cfb65690cd
5OO <git.hub@myrk.eu>
2024-01-12 12:52:33 +0200

    created a Contact entity in Contact.java

---
+++ src/main/java/org/tims/aliasnetwork/model/Contact.java
@@ -0,0 +1,38 @@
+package org.tims.aliasnetwork.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "code_name")
+    private String codeName;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "created_at")
+    private String createdAt;
+
+    @Column(name = "updated_at")
+    private String updatedAt;
+
+    @Column(name = "deleted_at")
+    private String deletedAt;
+}
