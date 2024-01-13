1550c2e18419498e7472b251ec5e7ba52b237674
5OO <git.hub@myrk.eu>
2024-01-13 21:29:03 +0200

    Added updateContact method in ContactController and ContactService to update an existing Contact's information. If the specified Contact does not exist, an empty Optional is returned.

--- src/main/java/org/tims/aliasnetwork/controller/ContactController.java
+++ src/main/java/org/tims/aliasnetwork/controller/ContactController.java
@@ -1,10 +1,12 @@
 package org.tims.aliasnetwork.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.tims.aliasnetwork.model.Contact;
 import org.tims.aliasnetwork.service.ContactService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/contacts")
@@ -22,6 +24,14 @@ public class ContactController {
     @PostMapping
     public Contact createContact(@RequestBody Contact contact) {
         return contactService.createContact(contact);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Optional<Contact>> updateContact(@PathVariable(value="id") Long id, @RequestBody Contact contactDetails) {
+        Optional<Contact> updatedContact = contactService.updateContact(id, contactDetails);
+        if (updatedContact.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updatedContact);
     }
 
 }
--- src/main/java/org/tims/aliasnetwork/service/ContactService.java
+++ src/main/java/org/tims/aliasnetwork/service/ContactService.java
@@ -2,10 +2,13 @@ package org.tims.aliasnetwork.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.tims.aliasnetwork.model.Contact;
 import org.tims.aliasnetwork.repository.ContactRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ContactService {
@@ -23,4 +26,17 @@ public class ContactService {
         return contactRepository.save(contact);
     }
 
-}
+    public Optional<Contact> updateContact(@PathVariable Long id, @RequestBody Contact contact) {
+
+        Contact existingContact = contactRepository.findById(id).orElse(null);
+        if (existingContact != null) {
+            existingContact.setFirstName(contact.getFirstName());
+            existingContact.setLastName(contact.getLastName());
+            existingContact.setCodeName(contact.getCodeName());
+            existingContact.setPhoneNumber(contact.getPhoneNumber());
+            return Optional.of(contactRepository.save(existingContact));
+        }
+        return Optional.empty();
+        }
+
+    }
