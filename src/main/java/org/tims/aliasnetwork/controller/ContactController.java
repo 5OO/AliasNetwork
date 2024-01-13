f0e66d7b769afe78e62889ca2bbdad7396e87f58
5OO <git.hub@myrk.eu>
2024-01-13 11:17:52 +0200

    created a REST API

---
+++ src/main/java/org/tims/aliasnetwork/controller/ContactController.java
@@ -0,0 +1,32 @@
+package org.tims.aliasnetwork.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.tims.aliasnetwork.model.Contact;
+import org.tims.aliasnetwork.repository.ContactRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/contacts")
+public class ContactController {
+
+    private final ContactRepository contactRepository;
+
+    public ContactController(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+    @GetMapping
+    public List<Contact> getAllContacts() {
+        return contactRepository.findAll();
+    }
+    @PostMapping
+    public Contact createContact(@RequestBody Contact contact) {
+        return contactRepository.save(contact);
+    }
+
+}
+
+
+
+
