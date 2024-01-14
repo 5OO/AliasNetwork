5c70926eea952a1bac0ea7f8a0f79716e32c341f
5OO <git.hub@myrk.eu>
2024-01-14 16:53:15 +0200

    Add ContactsList component
    Implemented a new Vue.js ContactsList component which displays a list of contacts fetched from the backend. Axios is used for making the HTTP request to the server, and the fetched data is integrated into the Vue.js data model.

---
+++ src/main/frontend/alias-network-app/src/components/ContactsList.vue
@@ -0,0 +1,39 @@
+
+
+<template>
+  <div>
+    <h1>Contacts</h1>
+    <ul>
+      <li v-for="contact in contacts" :key="contact.id">
+        {{contact.id}} {{ contact.firstName }} {{ contact.lastName }} - {{ contact.codeName }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+
+import axios from 'axios';
+export default {
+  name: 'ContactsList',
+  data() {
+    return {
+      contacts: []
+    };
+  },
+  mounted() {
+    // TODO: Fetch contacts from the backend and set this.contacts
+
+    axios.get('http://localhost:8080/api/contacts')
+        .then(response => {
+          this.contacts = response.data;
+        })
+        .catch(error => {
+          console.error('There was an error fetching the contacts:', error);
+        })
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
